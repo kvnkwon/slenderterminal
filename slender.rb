c6cf02fba0f53be72dae6171d84a0ae9d279b601
Kevin <kvnkwon@gmail.com>
2013-11-15 09:12:51 -0500

    first commit

---
+++ slender.rb
@@ -0,0 +1,147 @@
+puts "Welcome to Slenderman: The Terminal Edition v.1.0 BETA"
+puts "Created by Kevin Kwon."
+puts "To progress, press enter."
+gets
+puts "Bam."
+gets
+puts "Your car crashed. Day becomes night."
+gets
+puts "You stumble across a shady looking house."
+gets
+puts "What do you do?"
+gets
+puts "A. Ring the door bell."
+gets
+puts "B. Stay in your car."
+gets
+puts "C. Keep walking until you find another house."
+while true
+answer = gets.chomp.downcase
+
+while true
+if answer == "a"
+  puts "No one answers, but you find the door unlocked."
+  gets
+  puts "You decide to go in."
+  gets
+  puts "You call for help."
+  gets
+  puts "No one answers."
+  gets
+  puts "You approach the Kitchen, where you see the light is on."
+  gets
+  puts "There is a drawing on the counter. You take a look."
+  gets
+  puts "░░░░░░░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░░░░░░░
+░░░▄▄██░░▄░░░░░░░░░░█▀▀█░░░░▄▄░░▄░░░░░░░
+░░█▀░░█▄█▀░▄▄░░░░░░░█░░█░░░░██▄░█░█▀▀█░░
+░░░░░▀▀▀░█▀░█░░░░░░░█▄█▀░░░░█░███░█▄▄█░░
+░░▄▄██░░▄░▀▀▀░░░░░▄▄▄██▄░░░░▀░░▀▀░▀▀▀░░░
+░▄█▀░███▀▄▀█░░░▄███░░█░░██▄▄░░▄▄░█░▄▄▄░░
+░░░▄▄░░░░▀▄▀░░░░▀███░▀░▄███▀░░█▀██░█░█░░
+░▄█▀███▀▄██▄░░░░░████▄██▀██░░░█▄░█░█▄█░░
+░▀▀░▀▀░░▀██▀░░░░▄██▀███▀░██░░░██▄█░▄█▄▄░
+░░▄░▄▄░░▄▄▄░░░░░█▄▀░███░░░█▄░░▀░▀█░█▄▄█░
+▄██▄█▀░██▀█░░░░███░░███░░░██░░█▄░█░░░░░░
+▀░██░░██▄█░░░░░██░░░███░░░░█░░█▀██░█▀█░░
+░░░▄░▄▀▀▀░░░░░▄█░░░░███░░░░██░▀░░▀░▀▀▀░░
+░▄██▄█▄▀█░░░░░██░░░░███░░░█▀█░██░█░░░█▀█
+▀░░██░█▄█░░░░███░░░░█░█░░██░█░█▀██░░░█▄█
+░░░░░░░░░░░░██░█░░░▄█▀█▄░██░█░▀░▀▀░░░░░░
+░░░░░░░░░░░░░░██░░░██░██░▀░░██░██░█▄█▄▄░
+░░░░░░░░░░░░░░█▀░░▄█░░██▄░░░▀█░█▀███▄░░█
+░░░░░░░░░░░░░▀▀░░░█░░░░░█▄░░░░░█░▀█░█▄▄█"
+gets
+puts "The drawing is of a skinny figure in a suit. It looks horrifying."
+gets
+puts "Your observation is interrupted by a scream from outside. You decide to check it out."
+gets
+puts "Beta ends here. Thanks for playing."
+abort
+elsif answer == "b"
+  puts "As you wait in your car, you see heavy smoke rising from your engine."
+  gets
+  puts "You leave the car and you run to the house."
+  gets
+  puts "Before you reach the house, you see a figure near the porch."
+  gets
+  puts "You call for help."
+  gets
+  puts "The figure approaches you."
+  gets
+  puts "Something feels horribly wrong."
+  gets
+  puts "You better run."
+  gets
+    puts "░░░░░░▄▄░░░░░░░░░░░░░░░░░░░░░░░▀▀███░░░░
+░░▄▄███▄░░░░░░░░░░░░░░░░░░░░░░░░░▄███▄░░
+░░▄▄██▀▀░░░░░░░░▄█▀▀▀▀█▄░░░░░░▄██████▄░░
+█████████▄░░░░░░█░░░░░▀█░░░░░░░▄▄▄▀█▀▀░░
+░░▄███▀▀▀▀░░░░░░█░░░░░░█░░░░░░▀▀▀█████▄▄
+▄▄███████▀░░░░░░█▄░▀▀░▄█░░░░░░▄█▀███▄██░
+███████░░░░░░░░░░▀████▀░░░░░██████████▄▄
+███▄███▀▀▀░░░░░██▀▀██▀▀██░░░░░░░▀▀█████░
+░░████▄▄▄░░░░░█████░█░████░░░░░▀▀█████░░
+▀▀▀███▄▀▀░░░░███▀███░██▀███░░░░▀▀██████░
+░░▀█████▀░░░░███░██████░███░░░░░░░░█░░░░
+░░░░██░░░░░░░███░██████░███░░░░░░░░█░░░░
+░░░░█░░░░░░░░███░██████░███░░░░▄▄░░█░░░░
+░░░░█░░░░░░░░███░██████░███░░░░░░▀▀▀▄▄▄░
+░▀░▀▀▀▀▀░░░░░███░██████░███░░░░░░▄▄░░░░░
+░░▄▄░░░░░░░░░███░██████░███░░░░░████▄░░░
+░███░░░░░░░░░███░██████░███░░░░░░░░░░░░░
+░▀▀░░░░░░░░░░███░██████░███░░░░░░░░░░░░░
+░░░░░░░░░░░░░██▀░██▀▀██░▀██░░░░░░░░░░░░░"
+gets
+puts "Too late."
+puts "You are dead."
+  abort
+elsif answer == "c"
+  puts "The area surrounding you is a forest. There is nowhere to go."
+  gets
+  puts "If you follow the road, you know there will be nothing for many, many miles."
+  gets
+  puts "You decide to go into the forest. You feel like you're being chased."
+  gets
+  puts "You look behind you. There is nothing."
+  gets
+  puts "You don't feel safe at all..."
+  gets
+  puts "Someone is following you."
+  gets
+  puts "Don't look back."
+  gets
+  puts "Keep running."
+  gets
+  puts "You trip on a couple rocks. You look up in horror."
+  gets
+  puts "░░░░░░▄▄░░░░░░░░░░░░░░░░░░░░░░░▀▀███░░░░
+░░▄▄███▄░░░░░░░░░░░░░░░░░░░░░░░░░▄███▄░░
+░░▄▄██▀▀░░░░░░░░▄█▀▀▀▀█▄░░░░░░▄██████▄░░
+█████████▄░░░░░░█░░░░░▀█░░░░░░░▄▄▄▀█▀▀░░
+░░▄███▀▀▀▀░░░░░░█░░░░░░█░░░░░░▀▀▀█████▄▄
+▄▄███████▀░░░░░░█▄░▀▀░▄█░░░░░░▄█▀███▄██░
+███████░░░░░░░░░░▀████▀░░░░░██████████▄▄
+███▄███▀▀▀░░░░░██▀▀██▀▀██░░░░░░░▀▀█████░
+░░████▄▄▄░░░░░█████░█░████░░░░░▀▀█████░░
+▀▀▀███▄▀▀░░░░███▀███░██▀███░░░░▀▀██████░
+░░▀█████▀░░░░███░██████░███░░░░░░░░█░░░░
+░░░░██░░░░░░░███░██████░███░░░░░░░░█░░░░
+░░░░█░░░░░░░░███░██████░███░░░░▄▄░░█░░░░
+░░░░█░░░░░░░░███░██████░███░░░░░░▀▀▀▄▄▄░
+░▀░▀▀▀▀▀░░░░░███░██████░███░░░░░░▄▄░░░░░
+░░▄▄░░░░░░░░░███░██████░███░░░░░████▄░░░
+░███░░░░░░░░░███░██████░███░░░░░░░░░░░░░
+░▀▀░░░░░░░░░░███░██████░███░░░░░░░░░░░░░
+░░░░░░░░░░░░░██▀░██▀▀██░▀██░░░░░░░░░░░░░"
+  gets
+  puts "They call him, Slenderman."
+  gets
+  puts "You are dead."
+  abort
+else
+  puts "Please choose: a, b, or c."
+  break
+end
+end
+end
